948a251bad3481d560d33c1cd4549082ffd3b81d
ehlera1 <adam.p.ehlers@gmail.com>
2022-03-03 20:41:44 -0600

    Webapp revision 1734

--- Webapp_dev/Trails_Webapp/static/js/map.js
+++ Webapp_dev/Trails_Webapp/static/js/map.js
@@ -15,13 +15,13 @@ var controlLayers = L.control.layers(null, null, {
 // display Carto basemap tiles with light features and labels
 var light = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
-}).addTo(map); // EDIT - insert or remove ".addTo(map)" before last semicolon to display by default
+}); // EDIT - insert or remove ".addTo(map)" before last semicolon to display by default
 controlLayers.addBaseLayer(light, 'Light basemap');
 
 /* Stamen colored terrain basemap tiles with labels */
 var terrain = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
-}); // EDIT - insert or remove ".addTo(map)" before last semicolon to display by default
+}).addTo(map); // EDIT - insert or remove ".addTo(map)" before last semicolon to display by default
 controlLayers.addBaseLayer(terrain, 'Terrain basemap');
 
 let csvString;
@@ -65,7 +65,7 @@ function setupMap(includePredicate) {
     if(csvString) {
         useCsv();
     } else {
-        $.get('./trail_data_clean_copy.csv', (csv) => {
+        $.get('./webpage_summary_copy.csv', (csv) => {
             csvString = csv;
             useCsv();
         });
